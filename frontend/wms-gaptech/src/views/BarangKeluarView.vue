bcce9e69f38b0e6d30a76c479deb6f51549b0229
ekahanny <ekahnny@gmail.com>
2024-04-06 00:05:38 +0800

    menyelesaikan slicing laman barang keluar

--- frontend/wms-gaptech/src/views/BarangKeluarView.vue
+++ frontend/wms-gaptech/src/views/BarangKeluarView.vue
@@ -1,8 +1,326 @@
+<!-- eslint-disable vue/max-attributes-per-line -->
+<!-- eslint-disable vue/html-closing-bracket-newline -->
+<!-- eslint-disable vue/html-indent -->
 <template>
   <div>
-    <p class="flex justify-center items-center font-semibold text-3xl mt-9">
+    <p
+      class="flex justify-center font-mono items-center font-semibold text-3xl mt-10"
+    >
       BARANG KELUAR
     </p>
+    <div class="flex items-center place-content-between mt-12">
+      <div class="flex justify-center ml-10">
+        <label
+          for="entries"
+          class="mr-2 mt-3 text-sm font-medium leading-6 text-gray-900"
+          >Show</label
+        >
+        <div class="mt-2">
+          <select
+            id="entries"
+            name="entries"
+            class="w-16 rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+          >
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+          </select>
+        </div>
+        <p class="ml-2 mt-3 text-sm font-medium leading-6 text-gray-900">
+          Entries
+        </p>
+      </div>
+      <button
+        class="rounded-lg text-sm py-2 px-3 mr-8 bg-sky-900 text-white hover:bg-sky-950 flex items-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1"
+          stroke="currentColor"
+          class="w-4 h-4 border rounded-sm mr-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+
+        Tambah Data
+      </button>
+    </div>
+
+    <div class="container mx-auto">
+      <table class="w-full table-auto border-2 border-slate-400 rounded mt-10">
+        <thead>
+          <tr class="border-2 border-slate-400">
+            <th class="px-4 py-2 border border-gray-300 text-center">No</th>
+            <th class="px-4 py-2 border border-gray-300 text-center">
+              ID Transaksi
+            </th>
+            <th
+              class="px-4 py-2 border border-gray-200 text-center flex justify-center"
+            >
+              Tanggal Keluar
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 mt-1.5 ml-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+                />
+              </svg>
+            </th>
+            <th class="px-4 py-2 border border-gray-300 text-center">Action</th>
+          </tr>
+        </thead>
+        <tbody class="border-2 border-slate-400">
+          <tr>
+            <td class="px-4 py-2 border border-gray-300 text-center">1</td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              TK-0001
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              29-03-2024
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              <button
+                class="rounded-lg text-sm py-2 px-3 mr-1 bg-red-700 text-white hover:bg-red-900"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+              <button
+                class="rounded-lg text-sm py-2 px-3 bg-amber-400 text-white hover:bg-amber-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+          <tr class="bg-slate-100">
+            <td class="px-4 py-2 border border-gray-300 text-center">2</td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              TK-0002
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              28-03-2024
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              <button
+                class="rounded-lg text-sm py-2 px-3 mr-1 bg-red-700 text-white hover:bg-red-900"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+              <button
+                class="rounded-lg text-sm py-2 px-3 bg-amber-400 text-white hover:bg-amber-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td class="px-4 py-2 border border-gray-300 text-center">3</td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              TK-0003
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              27-03-2024
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              <button
+                class="rounded-lg text-sm py-2 px-3 mr-1 bg-red-700 text-white hover:bg-red-900"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+              <button
+                class="rounded-lg text-sm py-2 px-3 bg-amber-400 text-white hover:bg-amber-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+          <tr class="bg-slate-100">
+            <td class="px-4 py-2 border border-gray-300 text-center">4</td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              TK-0004
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              26-03-2024
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              <button
+                class="rounded-lg text-sm py-2 px-3 mr-1 bg-red-700 text-white hover:bg-red-900"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+              <button
+                class="rounded-lg text-sm py-2 px-3 bg-amber-400 text-white hover:bg-amber-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td class="px-4 py-2 border border-gray-300 text-center">5</td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              TK-0004
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              25-03-2024
+            </td>
+            <td class="px-4 py-2 border border-gray-300 text-center">
+              <button
+                class="rounded-lg text-sm py-2 px-3 mr-1 bg-red-700 text-white hover:bg-red-900"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+              <button
+                class="rounded-lg text-sm py-2 px-3 bg-amber-400 text-white hover:bg-amber-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
