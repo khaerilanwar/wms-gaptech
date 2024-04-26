849b32f48108c270447d5683b02bc4d8bab4888f
ekahanny <ekahnny@gmail.com>
2024-04-26 13:47:51 +0800

    router index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,52 +1,74 @@
-import express from 'express'
-import { Login, Logout, Register, getUser, getUsers } from '../controllers/Users.js'
-import { verifyToken } from '../middleware/VerifyToken.js'
-import { refreshToken } from '../controllers/RefreshToken.js'
-import { addProduct, addStock, getProduct, getProducts, updateProduct } from '../controllers/Products.js'
-import { getInProducts, inProductByMonth, inProductByPeriod, inProductLast30Days } from '../controllers/InProducts.js'
-import { develop } from '../controllers/Development.js'
-import { getOutProducts, outProductByMonth, outProductByPeriod, outProductLast30Days } from '../controllers/OutProducts.js'
-import { getAllTransactions } from '../controllers/Transaction.js'
+import express from "express";
+import {
+  Login,
+  Logout,
+  Register,
+  getUser,
+  getUsers,
+} from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
+import {
+  addProduct,
+  addStock,
+  getProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/Products.js";
+import {
+  getInProducts,
+  inProductByMonth,
+  inProductByPeriod,
+  inProductLast30Days,
+} from "../controllers/InProducts.js";
+// import { develop } from "../controllers/Development.js";
+import {
+  getOutProducts,
+  outProductByMonth,
+  outProductByPeriod,
+  outProductLast30Days,
+} from "../controllers/OutProducts.js";
+import { getAllTransactions } from "../controllers/Transaction.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // Router authentication
-router.get('/user', verifyToken, getUser)
-router.get('/users', verifyToken, getUsers)
-router.post('/register', Register)
-router.post('/login', Login)
-router.get('/token', refreshToken)
-router.delete('/logout', Logout)
+router.get("/user", verifyToken, getUser);
+router.get("/users", verifyToken, getUsers);
+router.post("/register", Register);
+router.post("/login", Login);
+router.get("/token", refreshToken);
+router.delete("/logout", Logout);
 
 // Router products
 // gunakan middleware verifyToken !!!!!!!!!!!!!!!!!!!!
-router.get('/products', verifyToken, getProducts)
-router.get('/product/:kodeProduk(\\d+)', verifyToken, getProduct) // kodeProduk hanya menerima numeric
-router.post('/product', verifyToken, addProduct)
-router.put('/product/:kodeProduk(\\d+)', verifyToken, updateProduct)
-router.patch('/product/:kodeProduk(\\d+)', verifyToken, addStock)
+router.get("/products", verifyToken, getProducts);
+router.get("/product/:kodeProduk(\\d+)", verifyToken, getProduct); // kodeProduk hanya menerima numeric
+router.post("/product", verifyToken, addProduct);
+router.put("/product/:kodeProduk(\\d+)", verifyToken, updateProduct);
+router.patch("/product/:kodeProduk(\\d+)", verifyToken, addStock);
 
 // Router in products (history)
-router.get('/inproducts', verifyToken, getInProducts)
-router.get('/inproducts/last30days', verifyToken, inProductLast30Days)
-router.get('/inproducts/data-by-period', verifyToken, inProductByPeriod)
-router.get('/inproducts/data-by-month', verifyToken, inProductByMonth)
+router.get("/inproducts", verifyToken, getInProducts);
+router.get("/inproducts/last30days", verifyToken, inProductLast30Days);
+router.get("/inproducts/data-by-period", verifyToken, inProductByPeriod);
+router.get("/inproducts/data-by-month", verifyToken, inProductByMonth);
 
 // Router out products (history)
-router.get('/outproducts', verifyToken, getOutProducts)
-router.get('/outproducts/last30days', verifyToken, outProductLast30Days)
-router.get('/outproducts/data-by-period', verifyToken, outProductByPeriod)
-router.get('/outproducts/data-by-month', verifyToken, outProductByMonth)
+router.get("/outproducts", verifyToken, getOutProducts);
+router.get("/outproducts/last30days", verifyToken, outProductLast30Days);
+router.get("/outproducts/data-by-period", verifyToken, outProductByPeriod);
+router.get("/outproducts/data-by-month", verifyToken, outProductByMonth);
 
 // Router transaction product
-router.get('/transactions', verifyToken, getAllTransactions)
+router.get("/transactions", verifyToken, getAllTransactions);
 
 // Router Debug Development
 // router.get('/dev/sayang', develop)
 
 router.use((req, res) => {
-    res.status(404)
-    res.send('<h1>404 Not Found Sayangku!!</h1>')
-})
+  res.status(404);
+  res.send("<h1>404 Not Found Sayangku!!</h1>");
+});
 
-export default router
+export default router;
