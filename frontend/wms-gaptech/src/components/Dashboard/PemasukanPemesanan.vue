6510bb0124810d80db8414a4d19d548c9629ec79
ekahanny <ekahnny@gmail.com>
2024-05-06 15:35:38 +0800

    fetch data for dashboard

--- frontend/wms-gaptech/src/components/Dashboard/PemasukanPemesanan.vue
+++ frontend/wms-gaptech/src/components/Dashboard/PemasukanPemesanan.vue
@@ -5,9 +5,8 @@
     >
       <h1 class="font-semibold xl:text-2xl">Pemasukan/bulan</h1>
       <div class="flex justify-between items-end">
-        <p class="xl:text-3xl">Rp</p>
         <p class="font-extrabold text-2xl lg:text-3xl xl:text-5xl">
-          100.000.000
+          {{ $filters.currency(monthlyData.income[4]) }}
         </p>
       </div>
       <!-- pengen nambahin kenaikan dibanding dengan minggu lalu -->
@@ -17,13 +16,59 @@
     >
       <h1 class="font-semibold xl:text-2xl">Transaksi/bulan</h1>
       <div class="flex justify-between items-end">
-        <p class="font-extrabold text-2xl lg:text-3xl xl:text-5xl">1.000</p>
+        <p class="font-extrabold text-2xl lg:text-3xl xl:text-5xl">
+          {{ monthlyData.transaction[4] }}
+        </p>
         <span class="xl:text-3xl">invoice</span>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script>
+import axiosInstance from "@/utils/api";
+
+export default {
+  data() {
+    return {
+      monthlyData: {
+        income: {},
+        transaction: {},
+      },
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axiosInstance.get("transactions");
+        this.monthlyReport(response.data);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching data : ", error);
+      }
+    },
+    monthlyReport(transactions) {
+      transactions.forEach((transaction) => {
+        const month = new Date(transaction.tanggalTransaksi).getMonth(); // Mengambil bulan dari 1 hingga 12
+        console.log(month);
+
+        if (!this.monthlyData.income[month]) {
+          this.monthlyData.income[month] = 0;
+        }
+        if (!this.monthlyData.transaction[month]) {
+          this.monthlyData.transaction[month] = 0;
+        }
+
+        this.monthlyData.income[month] += transaction.totalHarga;
+        this.monthlyData.transaction[month] += 1;
+      });
+      console.log("Monthly Data:", this.monthlyData);
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped></style>
